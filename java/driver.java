211cc1431a0e49336aa9225115c055f23c1bd78d
aidend <aidendawes@gmail.com>
2020-11-03 17:35:15 +0200

    Java Init

---
+++ java/driver.java
@@ -0,0 +1,9 @@
+package java;
+import java.Rectangle;
+public class driver {
+    
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle();
+        rec.printShape();
+    }
+}
